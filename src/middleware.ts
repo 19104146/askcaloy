4f6979ca01b5abc8891f2d57adbcd33ed2dcd496
19104146 <19104146@usc.edu.ph>
2024-12-05 08:59:45 +0800

    feat: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,3 +10,7 @@ export function middleware(request: NextRequest) {
 
     return response
 }
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
+}
