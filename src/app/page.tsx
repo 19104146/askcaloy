32c7c4d359f763ae2dae9da2e4453da247dde8bd
19104146 <19104146@usc.edu.ph>
2024-12-05 10:37:39 +0800

    feat: update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,18 @@ export default function Page() {
     const exampleUrl = "https://en.wikipedia.org/wiki/Society_of_the_Divine_Word"
 
     return (
-        <main className="flex min-h-screen items-center justify-center">
+        <section className="flex min-h-screen items-center justify-center">
             <div className="w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                <div className="mb-3 ml-1">
-                    Enter a website URL as the route to get started
-                    <Link className="block underline" href={`/${exampleUrl}`}>
+                <div className="mb-3 ml-1 text-center">
+                    Enter a website URL as the route to get started. For example:
+                    <Link
+                        className="relative mt-3 block rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold underline"
+                        href={`/${exampleUrl}`}
+                    >
                         {`${baseUrl}/${exampleUrl}`}
                     </Link>
                 </div>
             </div>
-        </main>
+        </section>
     )
 }
