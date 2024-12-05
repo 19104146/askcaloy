3ef30f9aecfc186f3583ae9b1342e53a503d5950
19104146 <19104146@usc.edu.ph>
2024-12-05 09:22:45 +0800

    feat: update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link"
 
 export default function Page() {
+    const baseUrl =
+        process.env.NODE_ENV === "production"
+            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+            : "http://localhost:3000"
+
+    const exampleUrl = "https://en.wikipedia.org/wiki/Society_of_the_Divine_Word"
+
     return (
         <main className="flex min-h-screen items-center justify-center">
             <div className="w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                 <div className="mb-3 ml-1">
                     Enter a website URL as the route to get started
-                    <Link className="block underline" href="/https://en.wikipedia.org/wiki/Society_of_the_Divine_Word">
-                        {"http://localhost:3000/https://en.wikipedia.org/wiki/Society_of_the_Divine_Word"}
+                    <Link className="block underline" href={`/${exampleUrl}`}>
+                        {`${baseUrl}/${exampleUrl}`}
                     </Link>
                 </div>
             </div>
